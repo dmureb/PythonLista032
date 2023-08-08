f26dd348df5a2ffcf72be3a4692c6aaab13c4fc0
Daniel Mureb <danielmureb@hotmail.com>
2023-08-08 11:29:29 -0300

    lista de exercicios pronta

--- questao10.py
+++ questao10.py
@@ -7,3 +7,8 @@ eleitores = int(input("Informe o número total de eleitores do município: "))
 brancos = int(input("Informe o número total de votos brancos: "))
 nulos = int(input("Informe o número total de votos nulos: "))
 validos = int(input("Informe o número total de votos válidos: "))
+
+print(f"Votos totais: {eleitores}%")
+print(f"Votos brancos: {(100 * brancos)/eleitores:.2f}%")
+print(f"Votos nulos: {(100 * nulos)/eleitores:.2f}%")
+print(f"Votos válidos: {(100 * validos)/eleitores:.2f}%")
